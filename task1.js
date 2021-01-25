c11a307ca4ee96d3aef2f10bd889a87b380afac5
AndriyStefishyn <123456789andrij@gmail.com>
2021-01-25 12:01:48 +0200

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,43 @@
+alert("Andriy");
+
+console.log("Stefyshyn");
+
+let x=2;
+let y=6;
+x=y;
+alert(x, y);
+
+
+let phone={
+Brand:"iphone",
+Model:12,
+isWorking:true,
+manufacturer:undefined,
+age:null,
+}
+
+let isAdult=confirm("How old are you?");
+console.log(isAdult);
+
+
+
+
+
+
+let login=prompt("What is your login?");
+alert(login);
+let email=prompt("What is your email?");
+alert(email);
+let password=prompt("What is your password");
+alert(password);
+
+let name1="Andriy";
+let surname="Stefyshyn";
+let group=1;
+let year=1998;
+isMerried=false;
+console.log (typeof(name1));
+console.log (typeof(surname));
+console.log (typeof(group));
+console.log (typeof(year));
+console.log (typeof(isMerried));
