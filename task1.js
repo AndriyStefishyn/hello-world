34ff5161abd980bc70eafc02eb205eb093ef7cc5
AndriyStefishyn <123456789andrij@gmail.com>
2021-01-25 15:29:13 +0200

    Add files via upload

--- task1.js
+++ task1.js
@@ -1,12 +1,17 @@
+//Task1
 alert("Andriy");
 
+//Task2
 console.log("Stefyshyn");
 
+//Task3
 let x=2;
 let y=6;
 x=y;
 alert(x, y);
 
+
+//Task
 
 let phone={
 Brand:"iphone",
@@ -16,21 +21,15 @@ manufacturer:undefined,
 age:null,
 }
 
+
+//Task5
 let isAdult=confirm("How old are you?");
 console.log(isAdult);
 
 
 
 
-
-
-let login=prompt("What is your login?");
-alert(login);
-let email=prompt("What is your email?");
-alert(email);
-let password=prompt("What is your password");
-alert(password);
-
+//Task6
 let name1="Andriy";
 let surname="Stefyshyn";
 let group=1;
@@ -41,3 +40,20 @@ console.log (typeof(surname));
 console.log (typeof(group));
 console.log (typeof(year));
 console.log (typeof(isMerried));
+
+
+//Task7
+let login=prompt("What is your login?");
+alert("Dear user,your login is"+login);
+let email=prompt("What is your email?");
+alert("Dear user,your email is"+email);
+let password=prompt("What is your password");
+alert("Dear user,your password is"+password);
+
+//Task8
+let minutes=60*60;
+alert(minutes);
+let hours=3600*24;
+alert(hours);
+let month=86400*30;
+alert(month);
