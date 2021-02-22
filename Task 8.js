3ec93cf10120211bc5b9e2457d0ee5a0f2e8799a
AndriyStefishyn <123456789andrij@gmail.com>
2021-02-22 11:48:19 +0200

    Add files via upload

---
+++ Task 8.js
@@ -0,0 +1,42 @@
+//Task 1
+/* function toUpper(str){
+    return /^[A-Z]/.test(str);
+    }
+console.log(toUpper("regexp"));
+console.log(toUpper("RegExp")); */
+
+//Task 2
+/* function checkEmail(str){
+    return /./g.test(str); 
+}
+        console.log(checkEmail("Qmail2@gmail.com")) */
+ 
+//Task 3
+/* let str ="cdbBdbsbz";
+let reg = /d(b+)(d)/i;
+console.log(reg.exec(str));  */
+ 
+
+ //Task 4
+/* let reg = "Java Script";
+let res = /\s/;
+let spl=reg.split(res);
+[spl[0],spl[1]]=[spl[1],spl[0]];
+console.log(spl); */
+
+
+
+//Task 5
+/* function validation(Number){
+    return /\d{4}-\d{4}-\d{4}-\d{4}/.test(Number);
+    
+    }
+console.log(validation("9999-9999-9999-9999")); */
+
+
+//Task 6
+/* function email(str){
+return /^[^-_|\W]\w-?/g.test(str);
+}
+console.log(email("my_mail@gmail.com"));
+console.log(email("#my_mail@gmail.com")); */
